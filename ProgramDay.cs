47a4ea3bebd5ba63e7ed688e8d2d3ce8ffda765e
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-03-09 14:27:34 +0530

    Add files via upload

---
+++ ProgramDay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int dayno;
+
+			dayno = Convert.ToInt32(Console.ReadLine());
+
+			switch (dayno)
+			{
+				case 1:
+					Console.Write("Monday");
+					break;
+				case 2:
+					Console.Write("Tuesday");
+					break;
+				case 3:
+					Console.Write("Wednesday");
+					break;
+				case 4:
+					Console.Write("Thursday");
+					break;
+				case 5:
+					Console.Write("Friday");
+					break;
+				case 6:
+					Console.Write("Saturday");
+					break;
+				case 7:
+					Console.Write("Sunday");
+					break;
+				default:
+					Console.Write("Invalid day number. \nPlease try again ....\n");
+					break;
+			}
+					Console.ReadKey();
+			}
+		}
+	}
