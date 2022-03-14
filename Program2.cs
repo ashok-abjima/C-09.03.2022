2763687703b9e2341862712d530354e759aaf4e5
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-03-14 22:46:39 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class My
+{
+    int age = 20;
+    string name = "Ashok";
+    float per = 85.4f;
+
+    public void ott(out string n,out int a,out float p)
+    {
+        n = name;
+        a = age;
+        p = per;
+    }
+   /* public void ott1( string n1,  int a1,  float p1)
+    {
+        n1 = name;
+        a1 = age;
+        p1 = per;
+    }*/
+}
+class Program
+{
+    static void Main()
+    {
+        My obj = new My();
+        string x;
+        int y;
+        float z;
+       // obj.ott1( x,  y,  z);
+        obj.ott(out x,out y,out z);
+        //String nam = s+" "+a;
+        //Console.WriteLine(nam);
+        Console.WriteLine("{0} {1} {2}",x,y,z);
+        Console.ReadKey();
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class My
+{
+    public int Sum(params int [] arr)
+    {
+        int ad=0;
+        for (int i=0; i<arr.Length; i++)
+        {
+            ad= ad + arr[i];
+
+        }
+        return ad;
+
+    }
+    public string Summ(params string[] ar)
+    {
+        string ad = " ";
+        for (int i = 0; i < ar.Length; i++)
+        {
+            ad = ad +" "+ ar[i];
+
+        }
+        return ad;
+
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        My ob = new My();
+        int x = ob.Sum(10, 20, 30, 40, 50, 60, 70);
+        int y = ob.Sum(10, 20, 30);
+        int z = ob.Sum(10, 20, 30, 40, 50, 60, 70,1,2,3,4,5,6,7,8,9,1);
+        Console.WriteLine("Sum is :{0}",x);
+        Console.WriteLine("Sum is :{0}", y);
+        Console.WriteLine("Sum is :{0}", z);
+        Console.ReadKey();
+        string t = ob.Summ("Ashok", "ak", "sdda");
+        Console.WriteLine("Summ is :{0}", t);
+        Console.ReadKey();
+    }
+}
